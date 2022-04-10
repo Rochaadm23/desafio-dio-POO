03c1600eb95e12d43a48c6d93157c5c7c316896e
Fernando Rocha <ribeiro.ribeiro821@gmail.com>
2022-04-09 22:54:55 -0300

    Final: Interligando as classes de dominio

--- src/br/com/dio/desafio/application/Program.java
+++ src/br/com/dio/desafio/application/Program.java
@@ -1,7 +1,8 @@
 package br.com.dio.desafio.application;
 
-import br.com.dio.desafio.dominio.Conteudo;
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -10,6 +11,7 @@ public class Program {
     public static void main(String[] args) {
         Curso curso1 = new Curso();
         Curso curso2 = new Curso();
+        Curso curso3 = new Curso();
 
         curso1.setTitle("Java");
         curso1.setDescription("Curso de programação JAVA");
@@ -19,22 +21,46 @@ public class Program {
         curso2.setDescription("Curso de estilos com CSS");
         curso2.setCargaHoraria(4);
 
-        System.out.println(curso1);
-        System.out.println(curso2);
+        curso3.setTitle("Curso Pentesting");
+        curso3.setDescription("Aprenda técnicas de Hacking");
+        curso3.setCargaHoraria(40);
 
         Mentoria mentoria1 = new Mentoria();
         mentoria1.setTitle("Mentoria JAVA");
         mentoria1.setDescription("Passando os bizus de java");
         mentoria1.setDate(LocalDate.now());
 
-        System.out.println(mentoria1);
+        Bootcamp bootcamp1 = new Bootcamp();
+        bootcamp1.setName("Bootcamp JAVA Developer");
+        bootcamp1.setDescription("Desenvolva suas habilidades de programado java");
+        bootcamp1.getConteudos().add(curso1);
+        bootcamp1.getConteudos().add(curso2);
+        bootcamp1.getConteudos().add(curso3);
+        bootcamp1.getConteudos().add(mentoria1);
 
-        Curso curso3 = new Curso();
-        curso3.setTitle("Curso Pentesting");
-        curso3.setDescription("Aprenda técnicas de Hacking");
-        curso3.setCargaHoraria(40);
+        Dev devRocha = new Dev();
+        devRocha.setName("Rocha");
+        devRocha.inscreverBootcamp(bootcamp1);
+        System.out.println("Conteúdos inscritos DEV: " + devRocha.getName() + devRocha.getConteudosInscritos());
+        devRocha.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos inscritos DEV: " + devRocha.getName() + devRocha.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos DEV: " + devRocha.getName() + devRocha.getConteudosConcluidos());
 
-        System.out.println(curso3);
+        System.out.println("---------------------------------------------------------------------------------");
+
+        Dev devBart = new Dev();
+        devBart.setName("Bart");
+        devBart.inscreverBootcamp(bootcamp1);
+        System.out.println("Conteúdos inscritos DEV: " + devBart.getName() + devBart.getConteudosInscritos());
+        devBart.progredir();
+        devBart.progredir();
+        devBart.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos inscritos DEV: " + devBart.getName() + devBart.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos DEV: " + devBart.getName() + devBart.getConteudosConcluidos());
+        System.out.println("XP: " + devBart.calcularTotalXp());
+
 
     }
 }
